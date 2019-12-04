f3d41e1d0d66ed1a72343ffa4abfc6ac32339319
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-12-04 15:11:46 +1300

    controller decorator

---
+++ build/controllers/decorators/routes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+function get(path) {
+    return function (target, key, desc) {
+        Reflect.defineMetadata("path", path, target, target[key]);
+    };
+}
+exports.get = get;
