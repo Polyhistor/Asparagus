35873c412c54a74fa64d0fe9044ba38cc5277c45
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-12-05 14:52:33 +1300

    finalized decorators for login auth

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from "express";
 import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { router as controllerRouter } from "./controllers/decorators/controller";
+import "./controllers/loginController";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["laskdj"] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
